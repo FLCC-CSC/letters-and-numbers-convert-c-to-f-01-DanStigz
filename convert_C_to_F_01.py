319804d620995de1b91035ce4ae02f17ec06e2ac
DanStigz <dstiglitz@fingerlakes.edu>
2025-02-18 20:27:33 -0500

    Update convert_C_to_F_01.py

--- convert_C_to_F_01.py
+++ convert_C_to_F_01.py
@@ -19,7 +19,7 @@
     
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
-celsius = float(input('Enter a temperature in Celsius:'))
+celsius = float(input('Enter a temperature in Celsius: '))
 
 fahrenheit= (celsius * 9/5 + 32)
 
